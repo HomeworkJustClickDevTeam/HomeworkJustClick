fc7e35ae78ca1f108f3cf1303bf65e3679cfc8f2
jbiesek <jedrzejbiesek@gmail.com>
2023-05-16 19:57:26 +0200

    Fix CORS; Fix add file

--- Backend/src/main/java/pl/HomeworkJustClick/Backend/Services/FileServiceImplement.java
+++ Backend/src/main/java/pl/HomeworkJustClick/Backend/Services/FileServiceImplement.java
@@ -26,6 +26,7 @@ public class FileServiceImplement implements FileService {
     SolutionRepository solutionRepository;
 
     EntityManager entityManager;
+    public FileServiceImplement(EntityManager entityManager) {this.entityManager = entityManager;};
 
     @Override
     public List<File> getAll() {
