c7cb3c325e1e8efc0ef8d157a5903fc328d774c6
XsedoX <91881545+XsedoX@users.noreply.github.com>
2023-06-05 10:17:34 +0200

    Evaluation docs done

--- Backend/src/main/java/pl/HomeworkJustClick/Backend/Auth/RegisterRequest.java
+++ Backend/src/main/java/pl/HomeworkJustClick/Backend/Auth/RegisterRequest.java
@@ -12,9 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @Schema(example = "Example")
     private String firstname;
+    @Schema(example = "Examplowski")
     private String lastname;
     @Schema(example = "dadwa@wda.com")
     private String email;
+    @Schema(example = "d092")
     private String password;
 }
