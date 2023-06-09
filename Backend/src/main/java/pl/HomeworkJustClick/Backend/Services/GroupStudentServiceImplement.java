fa1fae5bd578e27388fa9211472e2658db1331b0
jbiesek <jedrzejbiesek@gmail.com>
2023-06-09 14:36:27 +0200

    Add GroupTeacherRepository unit tests

--- Backend/src/main/java/pl/HomeworkJustClick/Backend/Services/GroupStudentServiceImplement.java
+++ Backend/src/main/java/pl/HomeworkJustClick/Backend/Services/GroupStudentServiceImplement.java
@@ -1,10 +1,8 @@
 package pl.HomeworkJustClick.Backend.Services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.HomeworkJustClick.Backend.Entities.GroupStudent;
-import pl.HomeworkJustClick.Backend.Entities.GroupTeacher;
 import pl.HomeworkJustClick.Backend.Repositories.GroupRepository;
 import pl.HomeworkJustClick.Backend.Repositories.GroupStudentRepository;
 import pl.HomeworkJustClick.Backend.Repositories.GroupTeacherRepository;
@@ -65,7 +63,7 @@ public class GroupStudentServiceImplement implements GroupStudentService{
 
     @Override
     public Boolean addStudentToGroup (int group_id, int student_id) {
-        int groupTeacherCheck = groupTeacherRepository.getGroupTeacherByTeacherAndGroup(student_id, group_id);
+        int groupTeacherCheck = groupTeacherRepository.checkForTeacherInGroup(student_id, group_id);
         int groupStudentCheck = groupStudentRepository.getGroupStudentByStudentAndGroup(student_id, group_id);
         if(groupTeacherCheck != 0 || groupStudentCheck != 0){
             return null;
