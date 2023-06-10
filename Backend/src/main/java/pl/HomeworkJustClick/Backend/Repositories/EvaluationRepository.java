1b0601a9193a0b290f78d6a67be39a7dcbe14404
jbiesek <jedrzejbiesek@gmail.com>
2023-06-10 11:56:00 +0200

    Add EvaluationRepository unit tests

--- Backend/src/main/java/pl/HomeworkJustClick/Backend/Repositories/EvaluationRepository.java
+++ Backend/src/main/java/pl/HomeworkJustClick/Backend/Repositories/EvaluationRepository.java
@@ -16,10 +16,10 @@ public interface EvaluationRepository extends JpaRepository<Evaluation, Integer>
     @Query(value = "select COUNT(e.*) from _evaluation e join _solution s on e.solution_id = s.id where s.assignment_id = :assignment_id", nativeQuery = true)
     public int countEvaluationsByAssignment(int assignment_id);
 
-    @Query(value = "select e.* from _evaluation e join _group_student gs on gs.group_id = e.group_id where gs.user_id = :student_id", nativeQuery = true)
+    @Query(value = "select e.* from _evaluation e join _solution s on e.solution_id = s.id where s.user_id = :student_id", nativeQuery = true)
     public List<Evaluation> getAllEvaluationsByStudent(int student_id);
 
-    @Query(value = "select e.* from _evaluation e join _group_student gs on gs.group_id = e.group_id where gs.user_id = :student_id and e.group_id = :group_id", nativeQuery = true)
+    @Query(value = "select e.* from _evaluation e join _solution s on e.solution_id = s.id where s.user_id = :student_id and e.group_id = :group_id", nativeQuery = true)
     public List<Evaluation> getAllEvaluationsByStudentInGroup(int student_id, int group_id);
 
     @Query(value = "select e.* from _evaluation e join _solution s on e.solution_id = s.id where s.id = :solution_id", nativeQuery = true)
