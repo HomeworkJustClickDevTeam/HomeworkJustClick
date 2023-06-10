e264bb8dde5d4394a1f008e6301c53ece77fbb13
Piotr Szymkowiak <piotr141097@gmail.com>
2023-06-10 20:04:34 +0200

    Added Button + HomeGuest styles

--- frontend/src/components/home/HomeGuest.tsx
+++ frontend/src/components/home/HomeGuest.tsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom"
-
+import "../button/button.css"
+import "./HomeGuest.css"
+import KolkoPoLewejStronie from './kolko_po_lewej_stronie.svg';
+import KolkoPoPrawejStronie from './kolko_po_prawej_stronie.svg';
 function HomeGuest() {
   return (
     <>
+      <div className="kolkoPoLewejStronie">
+        <img src={KolkoPoLewejStronie} alt="Kołko po lewej stronie" />
+      </div>
+      <div className="kolkoPoPrawejStronie">
+        <img src={KolkoPoPrawejStronie} alt="Kółko po prawej stronie" />
+      </div>
+      <div className="homeworkJustClick">Homework JustClick!</div>
+      <div className="prostoSzybkoWygodnie">Prosto, szybko, wygodnie!</div>
+      <div className="nieMaszJeszczeKonta">Nie masz jeszcze konta?</div>
       <Link to="/login">
-        <button type="button">Zaloguj się</button>
+        <button className="button-login-pos button-filled button-filled-text button-size" type="button">Zaloguj się</button>
       </Link>
       <Link to="/register">
-        <button type="button">Zarejestruj się</button>
+        <button className="button-register-pos button-outlined button-outlined-text button-size" type="button">Zarejestruj się</button>
       </Link>
     </>
   )
