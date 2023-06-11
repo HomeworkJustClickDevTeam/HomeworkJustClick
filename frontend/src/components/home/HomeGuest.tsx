c7abb009b6e75dd8b9a15f945eba33a49fc878f2
Piotr Szymkowiak <piotr141097@gmail.com>
2023-06-11 19:01:19 +0200

    Overflow fix

--- frontend/src/components/home/HomeGuest.tsx
+++ frontend/src/components/home/HomeGuest.tsx
@@ -3,6 +3,7 @@ import "../button/button.css"
 import "./HomeGuest.css"
 import KolkoPoLewejStronie from './kolko_po_lewej_stronie.svg';
 import KolkoPoPrawejStronie from './kolko_po_prawej_stronie.svg';
+
 function HomeGuest() {
   return (
     <>
