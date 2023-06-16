9c864d451cda578b6c17ba6473b42f33ce457e04
Maciej Matusz <maciej.matusz.szczecin@gmail.com>
2023-06-16 16:03:28 +0200

    solutions downloader start, fixes in filter solutions

--- frontend/src/components/assigments/AssigmentSpec.tsx
+++ frontend/src/components/assigments/AssigmentSpec.tsx
@@ -1,26 +1,27 @@
 import { useParams } from "react-router-dom"
 import { useContext, useEffect, useState } from "react"
 import postgresqlDatabase from "../../services/postgresDatabase"
-import {Assigment, Solution, userState} from "../../types/types"
+import { Assigment, Solution } from "../../types/types"
 import { parseISO } from "date-fns"
 
 import GroupRoleContext from "../../GroupRoleContext"
 import ModifyAssigment from "./ModifyAssigment"
 
 import AddSolution from "../solution/AddSolution"
-import Loading from "../animations/Loading";
-import userContext from "../../UserContext";
-import {AxiosError} from "axios";
-import {tr} from "date-fns/locale";
-import CheckedSolution from "../solution/CheckedSolution";
-import UncheckedSolution from "../solution/UncheckedSolution";
-import {type} from "os";
+import Loading from "../animations/Loading"
+import userContext from "../../UserContext"
+import { AxiosError } from "axios"
+
+import CheckedSolution from "../solution/CheckedSolution"
+import UncheckedSolution from "../solution/UncheckedSolution"
 
 function AssigmentSpec() {
   const { idAssigment, id } = useParams()
-  const {userState} = useContext(userContext)
+  const { userState } = useContext(userContext)
   const { role } = useContext(GroupRoleContext)
-  const [isSolutionChecked, setIsSolutionChecked] = useState<boolean | undefined>(undefined)
+  const [isSolutionChecked, setIsSolutionChecked] = useState<
+    boolean | undefined
+  >(undefined)
   const [solution, setSolution] = useState<Solution | undefined>(undefined)
   const [assignment, setAssignment] = useState<Assigment>({
     completionDatetime: new Date(),
@@ -34,25 +35,29 @@ function AssigmentSpec() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   useEffect(() => {
     postgresqlDatabase
-      .get(`/solution/getUncheckedSolutionByUserAssignmentGroup/${userState.userId}/${idAssigment}/${id}`)
+      .get(
+        `/solution/getUncheckedSolutionByUserAssignmentGroup/${userState.userId}/${idAssigment}/${id}`
+      )
       .then((response) => {
-        if(response.data.id !== null){
+        if (response.data.id !== null) {
           setIsLoading(false)
           setIsSolutionChecked(false)
           setSolution(response.data)
-          return void(0)
+          return void 0
         }
       })
-      .catch((error:AxiosError) => console.log(error))
+      .catch((error: AxiosError) => console.log(error))
       .finally(() => {
         postgresqlDatabase
-          .get(`/solution/getCheckedSolutionByUserAssignmentGroup/${userState.userId}/${idAssigment}/${id}`)
+          .get(
+            `/solution/getCheckedSolutionByUserAssignmentGroup/${userState.userId}/${idAssigment}/${id}`
+          )
           .then((response) => {
             if (response.data.id !== null) {
               setIsLoading(false)
               setIsSolutionChecked(true)
               setSolution(response.data)
-              return void (0)
+              return void 0
             }
           })
           .catch((error: AxiosError) => console.log(error))
@@ -62,7 +67,10 @@ function AssigmentSpec() {
               .then((response) => {
                 const responseData = response.data
                 const parsedDate = parseISO(responseData.completionDatetime)
-                setAssignment({ ...responseData, completionDatetime: parsedDate })
+                setAssignment({
+                  ...responseData,
+                  completionDatetime: parsedDate,
+                })
               })
               .catch((error) => {
                 console.log("Error fetching assignment:", error)
@@ -73,22 +81,18 @@ function AssigmentSpec() {
   if (isLoading) {
     return <Loading />
   }
+  console.log(solution)
   return (
     <>
       {role === "Teacher" ? (
         <ModifyAssigment assignment={assignment} />
-        ) : (
-        (typeof solution === undefined) ? (
-          <AddSolution assignment={assignment}/>
-          ) : (
-            isSolutionChecked ? (
-            <CheckedSolution assignment={assignment} solution={solution}/>
-            ) : (
-              <UncheckedSolution assignment={assignment} solution={solution}/>
-            )
-          )
-        )
-      }
+      ) : typeof solution === undefined ? (
+        <AddSolution assignment={assignment} />
+      ) : isSolutionChecked ? (
+        <CheckedSolution assignment={assignment} solution={solution} />
+      ) : (
+        <UncheckedSolution assignment={assignment} solution={solution} />
+      )}
     </>
   )
 }
