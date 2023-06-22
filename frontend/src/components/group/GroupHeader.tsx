bf4aa47ad2e90e40cb5a5c2d0eeedbaafd52cb96
Piotr Szymkowiak <piotr141097@gmail.com>
2023-06-22 17:32:00 +0200

    fix colors 2

--- frontend/src/components/group/GroupHeader.tsx
+++ frontend/src/components/group/GroupHeader.tsx
@@ -22,7 +22,7 @@ function GroupHeader({ group }: PropsForGroupItem) {
     <div>
         <div className='relative flex text-white justify-center'>
             <div className={`w-[85%] mt-8 h-36 rounded-xl ${colorsArray[group.color]}`}>
-            {/*<div className='w-[85%] mt-8 h-36 rounded-xl bg-main_blue'>*/}
+            {/* <div className='w-[85%] mt-8 h-36 rounded-xl bg-[#59007f] bg-[#006400] bg-[#800000] bg-[#9A6324] bg-[#469990] bg-[#000075] bg-[#f58231] bg-[#546A7B] bg-[#000000] bg-[#BB9F06] bg-[#3F2A2B] bg-[#565656] bg-[#99621E] bg-[#739E82] bg-[#550C18] bg-[#136F63] bg-[#EF7A85] bg-[#D664BE] bg-[#642CA9] bg-[#03312E]'> */}
                 <article className='absolute bottom-0 mb-6 ml-4'>
                     <p className='text-3xl'>{group.name}</p>
                     <p className='text-xl mt-1'>{group.description}</p>
