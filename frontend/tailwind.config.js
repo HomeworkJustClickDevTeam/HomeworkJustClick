1aab92fad2f1a357915ab1fecb0b92d115de9854
Piotr Szymkowiak <piotr141097@gmail.com>
2023-06-22 16:40:36 +0200

    colors fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       'berry_red': '#990000',
       black: colors.black,
       current: 'currentColor',
-      'colorsArray':['#59007f', '#006400']
+      'colorsArray':['bg-[#59007f]', 'bg-[#006400]', 'bg-[#800000]','bg-[#9A6324]', 'bg-[#469990]', 'bg-[#000075]', 'bg-[#f58231]', 'bg-[#546A7B]', 'bg-[#546A7B]','bg-[#432E36]','bg-[#3F2A2B]', 'bg-[#565656]', 'bg-[#99621E]', 'bg-[#739E82]' , 'bg-[#550C18]', 'bg-[#136F63]', 'bg-[#EF7A85]','bg-[#D664BE]','bg-[#642CA9]','bg-[#03312E]']
     },
     extend: {
       fontFamily: {
