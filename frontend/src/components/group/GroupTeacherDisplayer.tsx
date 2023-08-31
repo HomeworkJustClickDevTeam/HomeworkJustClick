b18d2a33a19802112eb39bf9e1fcec7b1058459b
XsedoX <julsad3@st.amu.edu.pl>
2023-08-31 07:34:39 +0200

    fixes

--- frontend/src/components/group/GroupTeacherDisplayer.tsx
+++ frontend/src/components/group/GroupTeacherDisplayer.tsx
@@ -13,7 +13,7 @@ function GroupTeacherDisplayer({groupId}: { groupId: string }) {
         setTeachers(teacher)
       })
       .catch((e) => console.log(e))
-  },)
+  },[])
   return (
     <div className='flex inline-block mt-4 border-b-2 border-b-light_gray w-fit'>
       <h1 className='font-bold text-2xl align-text-bottom mr-3'>Prowadzący: </h1>
