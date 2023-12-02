01ac3979ac8fc49ebe3e46086c328ec9a4a93d8c
jbiesek <jedrzejbiesek@gmail.com>
2023-12-02 10:55:42 +0100

    add comment_file_* color change by commentId

---
+++ Backend/src/main/java/pl/HomeworkJustClick/Backend/commentfiletext/CommentFileTextUpdateColorDto.java
@@ -0,0 +1,16 @@
+package pl.HomeworkJustClick.Backend.commentfiletext;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentFileTextUpdateColorDto {
+    @Size(max = 10)
+    @NotEmpty
+    private String color;
+}
