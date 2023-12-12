b6f906d23f08973705cf33dfa5581ccc0aa4310a
Juliusz Sadowski <julsad3@st.amu.edu.pl>
2023-12-12 21:02:24 +0100

    group table finished

--- frontend/src/components/report/ReportGroupTable.tsx
+++ frontend/src/components/report/ReportGroupTable.tsx
@@ -2,19 +2,41 @@ import {GroupReportModel} from "../../types/GroupReport.model";
 import {useAppSelector} from "../../types/HooksRedux";
 import {selectGroup} from "../../redux/groupSlice";
 import {useGetAssignmentsByGroup} from "../customHooks/useGetAssignmentsByGroup";
+import {useEffect, useState} from "react";
 
 export const ReportGroupTable = ({groupReport}:{groupReport: GroupReportModel}) =>{
-
-  const distinctStudents = () =>{
-    const tempArray = groupReport.assignments.map((assignmentReport)=>{
-      if(assignmentReport.students !== null){
-        return assignmentReport.students.map((studentResult)=>{
-          return {firstname: studentResult.student.firstname, lastname: studentResult.student.lastname}
+  const [studentsFullNames, setStudentsFullNames] = useState<Set<string>>(new Set)
+  const [studentsResults] = useState<Map<string,Map<string, number|undefined>>>(()=>{
+    const output = new Map<string, Map<string, number|undefined>>()
+    groupReport.assignments
+      .filter((assignmentReport)=>assignmentReport.students !== null)
+      .forEach(assignmentReport => {
+        let studentResultMap = new Map<string, number|undefined>()
+        assignmentReport.students.forEach((studentResult)=>{
+          const studentFullName = studentResult.student.firstname + " " + studentResult.student.lastname
+          if(!studentsFullNames.has(studentFullName)) setStudentsFullNames(prevState => new Set(prevState.add(studentFullName)))
+          studentResultMap.set(studentFullName, studentResult.result)
         })
-      }
+        output.set(assignmentReport.assignment.title, studentResultMap)
+      })
+    return output
     })
-    return [... new Map]
+  const renderStudentsFullNames = ():JSX.Element[] =>{
+    let studentsFullNamesColumn:JSX.Element[] = []
+    studentsFullNames.forEach(( studentFullName)=>{
+      studentsFullNamesColumn.push(<th>{studentFullName}</th>)
+    })
+    return studentsFullNamesColumn
   }
+
+  const renderStudentsResults = (assignmentTitle:string):JSX.Element[] =>{
+    let studentsResultsNumbersColumn:JSX.Element[]=[]
+    studentsFullNames.forEach((studentFullName)=>{
+      studentsResultsNumbersColumn.push(<td>{studentsResults.get(assignmentTitle)!.get(studentFullName)}</td>)
+    })
+    return studentsResultsNumbersColumn
+  }
+
   return (<table>
     <tbody>
       <tr>
@@ -23,28 +45,23 @@ export const ReportGroupTable = ({groupReport}:{groupReport: GroupReportModel}) 
         <th>Najwyższy wynik</th>
         <th>Najniższy wynik</th>
         <th>Średnia</th>
-        <th>Uczniowie</th>
-        {groupReport.assignments.map((assignmentReport)=>{
-          if(assignmentReport.students === null) return null
-          return assignmentReport.students.map((studentResult) => {
-            return <th>{studentResult.student.firstname} {studentResult.student.lastname}</th>
-          })
-        })}
+        <th>Studenci</th>
+        {renderStudentsFullNames()}
       </tr>
-      {groupReport.assignments.map((assignmentReport) => {
-        return(
-          <tr>
-            <td>{assignmentReport.assignment.title}</td>
-            <td>{assignmentReport.assignment.max_points}</td>
-            <td>{assignmentReport.maxResult}</td>
-            <td>{assignmentReport.minResult}</td>
-            <td>{assignmentReport.avgResult} ({assignmentReport.avgResultPercent}%)</td>
-            <td></td>
-            {assignmentReport.students !== null && assignmentReport.students.map((studentResult)=>{
-              return <td>{studentResult.result}</td>
-            })}
-          </tr>
-        )
+      {groupReport.assignments
+        .filter((assignmentReport)=> assignmentReport.students!==null)
+        .map((assignmentReport) => {
+          return(
+            <tr>
+              <td>{assignmentReport.assignment.title}</td>
+              <td>{assignmentReport.assignment.max_points}</td>
+              <td>{assignmentReport.maxResult}</td>
+              <td>{assignmentReport.minResult}</td>
+              <td>{assignmentReport.avgResult} ({assignmentReport.avgResultPercent}%)</td>
+              <td></td>
+              {renderStudentsResults(assignmentReport.assignment.title)}
+            </tr>
+          )
       })}
     </tbody>
   </table>)
