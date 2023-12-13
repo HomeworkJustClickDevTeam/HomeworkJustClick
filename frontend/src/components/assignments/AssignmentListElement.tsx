4ef670e42b0df312b599a52d85cd07a136c9b7d2
Juliusz Sadowski <julsad3@st.amu.edu.pl>
2023-12-13 18:44:30 +0100

    students can see statistics

--- frontend/src/components/assignments/AssignmentListElement.tsx
+++ frontend/src/components/assignments/AssignmentListElement.tsx
@@ -1,22 +1,29 @@
 import { Link } from "react-router-dom"
 import { format } from "date-fns"
 import { AssignmentPropsInterface } from "../../types/AssignmentPropsInterface"
+import {AssignmentInterface} from "../../types/AssignmentInterface";
+import {AssignmentPieChart} from "../evaluation/AssignmentPieChart";
+import React from "react";
 
 interface AssignmentItemPropsInterface extends AssignmentPropsInterface {
   idGroup?: string
   optionalUserId?: string
   createReportButton?: boolean
   handleGenerateReportButtonClick?: () => void
+  unfoldedPieChartAssignment?: AssignmentInterface
+  handleAssignmentClick?: (clickedAssignment: AssignmentInterface) => void
 }
 
 function AssignmentListElement({assignment,
+                                 unfoldedPieChartAssignment,
+                                 handleAssignmentClick,
                                  idGroup,
                                  optionalUserId,
                                  createReportButton=false,
                                  handleGenerateReportButtonClick}: AssignmentItemPropsInterface) {
   return (
     <>
-      <Link to={idGroup!==undefined ? `/group/${idGroup}/assignment/${assignment.id}`: "#"} state={optionalUserId}
+      <Link to={idGroup!==undefined ? `/group/${idGroup}/assignment/${assignment.id}`: "#"} onClick={() => {handleAssignmentClick && handleAssignmentClick(assignment)}} state={optionalUserId}
             className="flex ml-[7.5%] mt-4 border-border_gray border h-16 rounded-lg font-lato text-xl items-center justify-between">
         <div className="flex-col ml-10 ">
           <div>{assignment.title}</div>
@@ -25,6 +32,7 @@ function AssignmentListElement({assignment,
         {createReportButton && handleGenerateReportButtonClick && <Link to={`#`} type={"button"} onClick={()=>{handleGenerateReportButtonClick()}}>Wygeneruj raport</Link>}
         <div className="mr-10 font-semibold text-[28px]">{"/" + assignment.max_points}</div>
       </Link>
+      {unfoldedPieChartAssignment?.id === assignment.id && <div><br/><AssignmentPieChart assignment={assignment}/></div>}
     </>
   )
 }
--- frontend/src/components/evaluation/EvaluationsStudentPage.tsx
+++ frontend/src/components/evaluation/EvaluationsStudentPage.tsx
@@ -2,14 +2,23 @@ import {useGetAssignmentsByGroup} from "../customHooks/useGetAssignmentsByGroup"
 import {useAppSelector} from "../../types/HooksRedux";
 import {selectGroup} from "../../redux/groupSlice";
 import AssignmentListElement from "../assignments/AssignmentListElement";
+import {useState} from "react";
+import {AssignmentInterface} from "../../types/AssignmentInterface";
 
 export const EvaluationsStudentPage = () =>{
   const group = useAppSelector(selectGroup)
   const assignments = useGetAssignmentsByGroup(group!.id)
+  const [unfoldedPieChartAssignment, setUnfoldedPieChartAssignment] = useState<AssignmentInterface|undefined>(undefined)
+
+  const handleAssignmentClick = (clickedAssignment: AssignmentInterface) =>{
+    if(clickedAssignment.id === unfoldedPieChartAssignment?.id) setUnfoldedPieChartAssignment(undefined)
+    else setUnfoldedPieChartAssignment(clickedAssignment)
+  }
+
   return (
     <div>
       {assignments.map((assignment)=>{
-        return <AssignmentListElement assignment={assignment} key={assignment.id}/>
+        return <AssignmentListElement assignment={assignment} key={assignment.id} unfoldedPieChartAssignment={unfoldedPieChartAssignment} handleAssignmentClick={handleAssignmentClick}/>
       })}
    </div>)
 }
