1d52fa950d55285d49e83251f99b8e40f2b9f777
Piotr Szymkowiak <piotr141097@gmail.com>
2023-12-21 02:19:58 +0100

    Fix logo position + update favicon

--- frontend/src/components/header/HeaderLoggedOutState.tsx
+++ frontend/src/components/header/HeaderLoggedOutState.tsx
@@ -4,8 +4,8 @@ import Logo from "./Logo.svg"
 function HeaderLoggedOutState() {
   return (
     <div id={"headerLoggedOut"} className='relative flex h-[32px] xl:h-[64px] bg-main_blue items-center justify-center select-none'>
-      <Link to="/"> 
-        <img className="scale-50 relative top-[6%] xl:scale-100 xl:transform-none" src={Logo} alt="Logo"></img>
+      <Link to="/" className="scale-50 relative top-[6%] xl:scale-100 xl:transform-none"> 
+        <img src={Logo} alt="Logo"></img>
       </Link>
     </div>
   )
