a8cbe512da9db1144c71849a71348c78ec8b1e09
jbiesek <jedrzejbiesek@gmail.com>
2023-12-28 19:01:57 +0100

    add EvaluationController tests

--- Backend/src/test/java/pl/homeworkjustclick/evaluation/EvaluationControllerTest.java
+++ Backend/src/test/java/pl/homeworkjustclick/evaluation/EvaluationControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import pl.homeworkjustclick.BaseTestEntity;
+import pl.homeworkjustclick.assignment.AssignmentRepository;
+import pl.homeworkjustclick.evaluationreport.EvaluationReportRepository;
 import pl.homeworkjustclick.group.GroupRepository;
 import pl.homeworkjustclick.groupteacher.GroupTeacherRepository;
 import pl.homeworkjustclick.solution.SolutionRepository;
@@ -48,6 +50,10 @@ class EvaluationControllerTest extends BaseTestEntity {
     SolutionService solutionService;
     @Autowired
     GroupTeacherRepository groupTeacherRepository;
+    @Autowired
+    AssignmentRepository assignmentRepository;
+    @Autowired
+    EvaluationReportRepository evaluationReportRepository;
 
     private static Stream<Arguments> prepareValidData() {
         return Stream.of(
@@ -219,5 +225,335 @@ class EvaluationControllerTest extends BaseTestEntity {
                 .solutionId(solutionId)
                 .groupId(groupId)
                 .build();
+    }
+
+    @Test
+    void shouldGetEvaluationById() throws Exception {
+        var evaluation = evaluationRepository.findAll().get(0);
+        mockMvc.perform(get("/api/evaluations/{id}", evaluation.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(evaluation.getId()))
+                .andExpect(jsonPath("$.result").value(evaluation.getResult()))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationById() throws Exception {
+        mockMvc.perform(get("/api/evaluations/{id}", 9999))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationExtendedById() throws Exception {
+        var evaluation = evaluationRepository.findAll().get(0);
+        mockMvc.perform(get("/api/extended/evaluations/{id}", evaluation.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(evaluation.getId()))
+                .andExpect(jsonPath("$.result").value(evaluation.getResult()))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationExtendedById() throws Exception {
+        mockMvc.perform(get("/api/extended/evaluations/{id}", 9999))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationsByStudent() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        mockMvc.perform(get("/api/evaluations/byStudent/{id}", student.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationsByStudent() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluations/byStudent/{id}", student.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetExtendedEvaluationsByStudent() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        mockMvc.perform(get("/api/extended/evaluations/byStudent/{id}", student.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetExtendedEvaluationsByStudent() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/extended/evaluations/byStudent/{id}", student.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationsByAssignment() throws Exception {
+        var assignment = assignmentRepository.findAll().get(0);
+        mockMvc.perform(get("/api/evaluations/byAssignment/{id}", assignment.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationsByAssignment() throws Exception {
+        var assignment = assignmentRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluations/byAssignment/{id}", assignment.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetExtendedEvaluationsByAssignment() throws Exception {
+        var assignment = assignmentRepository.findAll().get(0);
+        mockMvc.perform(get("/api/extended/evaluations/byAssignment/{id}", assignment.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetExtendedEvaluationsByAssignment() throws Exception {
+        var assignment = assignmentRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/extended/evaluations/byAssignment/{id}", assignment.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationBySolution() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        mockMvc.perform(get("/api/evaluation/bySolution/{id}", solution.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.solutionId").value(solution.getId()))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationBySolution() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluation/bySolution/{id}", solution.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetExtendedEvaluationBySolution() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        mockMvc.perform(get("/api/extended/evaluation/bySolution/{id}", solution.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.solution.id").value(solution.getId()))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetExtendedEvaluationBySolution() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/extended/evaluation/bySolution/{id}", solution.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationsByGroupId() throws Exception {
+        var group = groupRepository.findAll().get(0);
+        mockMvc.perform(get("/api/evaluation/byGroupId/{id}", group.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(4))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationsByGroupId() throws Exception {
+        var group = groupRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluation/byGroupId/{id}", group.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0))
+                .andReturn();
+    }
+
+    @Test
+    void shouldCheckForEvaluationToSolutionAndReturnTrue() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        mockMvc.perform(get("/api/evaluation/checkForEvaluationToSolution/{id}", solution.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("true"))
+                .andReturn();
+    }
+
+    @Test
+    void shouldCheckForEvaluationToSolutionAndReturnFalse() throws Exception {
+        var solution = solutionRepository.findAll().get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluation/checkForEvaluationToSolution/{id}", solution.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("false"))
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetEvaluationsByStudentAndGroup() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByStudentId(student.getId()).get(0);
+        mockMvc.perform(get("/api/evaluations/byStudentAndGroup/{studentId}/{groupId}", student.getId(), group.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetEvaluationsByStudentAndGroup() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByStudentId(student.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/evaluations/byStudentAndGroup/{studentId}/{groupId}", student.getId(), group.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldGetExtendedEvaluationsByStudentAndGroup() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByStudentId(student.getId()).get(0);
+        mockMvc.perform(get("/api/extended/evaluations/byStudentAndGroup/{studentId}/{groupId}", student.getId(), group.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andReturn();
+    }
+
+    @Test
+    void shouldNotGetExtendedEvaluationsByStudentAndGroup() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByStudentId(student.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        mockMvc.perform(get("/api/extended/evaluations/byStudentAndGroup/{studentId}/{groupId}", student.getId(), group.getId()))
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    void shouldAddEvaluationWithTeacherAndSolution() throws Exception {
+        var teacher = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByTeacherId(teacher.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, teacher.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", teacher.getId(), solution.getId())
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.grade").value("5.0"))
+                .andReturn();
+        assertEquals(1, evaluationRepository.findAll().size());
+    }
+
+    @Test
+    void shouldNotAddEvaluationWithStudentAndSolution() throws Exception {
+        var student = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByStudentId(student.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, student.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", student.getId(), solution.getId())
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertEquals(0, evaluationRepository.findAll().size());
+    }
+
+    @Test
+    void shouldNotAddDuplicatedEvaluationWithTeacherAndSolution() throws Exception {
+        var size = evaluationRepository.findAll().size();
+        var teacher = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByTeacherId(teacher.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, teacher.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", teacher.getId(), solution.getId())
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertEquals(size, evaluationRepository.findAll().size());
+    }
+
+    @Test
+    void shouldNotAddEvaluationWithNotExistingTeacher() throws Exception {
+        var teacher = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByTeacherId(teacher.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, teacher.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", 9999, solution.getId())
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertEquals(0, evaluationRepository.findAll().size());
+    }
+
+    @Test
+    void shouldNotAddEvaluationWithNotExistingSolution() throws Exception {
+        var teacher = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByTeacherId(teacher.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, teacher.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", teacher.getId(), 9999)
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertEquals(0, evaluationRepository.findAll().size());
+    }
+
+    @Test
+    void shouldNotAddEvaluationWithNotExistingTeacherAndSolution() throws Exception {
+        var teacher = userRepository.findByEmail("jan_kowalski@gmail.com").get();
+        var group = groupRepository.getGroupsByTeacherId(teacher.getId()).get(0);
+        var solution = solutionRepository.getSolutionsByGroupId(group.getId()).get(0);
+        evaluationReportRepository.deleteAll();
+        evaluationRepository.deleteAll();
+        var body = objectMapper.writeValueAsString(createEvaluationDto(5.0, 5.0, false, teacher.getId(), solution.getId(), group.getId()));
+        mockMvc.perform(post("/api/evaluation/withUserAndSolution/{userId}/{solutionId}", 9999, 9999)
+                        .content(body)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        assertEquals(0, evaluationRepository.findAll().size());
     }
 }
