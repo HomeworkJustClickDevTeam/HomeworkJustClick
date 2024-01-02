d8670dc9b4ef0ca01bd741852d50203a04a5033a
jbiesek <jedrzejbiesek@gmail.com>
2024-01-02 19:00:15 +0100

    possible ssl fix

--- Backend(MongoDB)/src/main/java/pl/homeworkjustclick/infrastructure/rest/WebClientConfig.java
+++ Backend(MongoDB)/src/main/java/pl/homeworkjustclick/infrastructure/rest/WebClientConfig.java
@@ -1,11 +1,18 @@
 package pl.homeworkjustclick.infrastructure.rest;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
 
 @Configuration
 @RequiredArgsConstructor
@@ -13,10 +20,17 @@ public class WebClientConfig {
     private final PostgresConfigProps postgresConfigProps;
 
     @Bean
-    public WebClient postgresWebClient() {
+    public WebClient postgresWebClient() throws SSLException {
+        SslContext context = SslContextBuilder.forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(context));
+
         return WebClient.builder()
                 .baseUrl(postgresConfigProps.getUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
