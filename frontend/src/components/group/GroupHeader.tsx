2913d4f79e35ead5cbdf4777f9aaeffe3bb9c384
Piotr Szymkowiak <piotr141097@gmail.com>
2024-01-06 21:21:43 +0100

    Add scaling + add button + new dropdown

--- frontend/src/components/group/GroupHeader.tsx
+++ frontend/src/components/group/GroupHeader.tsx
@@ -5,7 +5,7 @@ import Loading from "../animations/Loading"
 import { selectGroup } from "../../redux/groupSlice"
 import { selectRole } from "../../redux/roleSlice"
 import { useAppSelector } from "../../types/HooksRedux"
-import { FaCaretDown, FaCog } from "react-icons/fa";
+import { FaCog } from "react-icons/fa";
 import { GroupHeaderSortButtonValues } from "../../types/GroupHeaderStortButtonValues";
 
 //import {theme.colors.colorsArray: string[]} = require("../../../tailwind.config")
@@ -32,7 +32,7 @@ function GroupHeader() {
   const group = useAppSelector(selectGroup)
   const role = useAppSelector(selectRole)
   const [sortButtonState, setSortButtonState] = useState<GroupHeaderSortButtonValues>(role === "Teacher" ? GroupHeaderSortButtonValues.uncheck : GroupHeaderSortButtonValues.toDo)
-  const options = role === "student" ? studentOptions : teacherOptions
+  const options = role === "Student" ? studentOptions : teacherOptions
   const [selectedOption, setSelectedOption] = useState(options[0])
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
@@ -79,43 +79,46 @@ function GroupHeader() {
     return (
       <div>
         <div className='relative flex text-white justify-center select-none'>
-          <div className={`relative select-none w-[85%] mt-8 h-36 rounded-xl ${colorsArray[group?.color]}`}>
+          <div className={`relative select-none w-[85%] mt-8 h-28 xl:h-36 rounded-xl ${colorsArray[group?.color]}`}>
             {/* <div className='w-[85%] mt-8 h-36 rounded-xl bg-[#59007f] bg-[#006400] bg-[#800000] bg-[#9A6324] bg-[#469990] bg-[#000075] bg-[#f58231] bg-[#546A7B] bg-[#000000] bg-[#BB9F06] bg-[#3F2A2B] bg-[#565656] bg-[#99621E] bg-[#739E82] bg-[#550C18] bg-[#136F63] bg-[#EF7A85] bg-[#D664BE] bg-[#642CA9] bg-[#03312E]'> */}
-            <article className='absolute bottom-0 mb-8 ml-4'>
-              <p className='text-3xl'>{group.name}</p>
-              <p className='text-xl mt-1'>{group.description}</p>
+            <article className='absolute bottom-0 mb-6 xl:mb-8 ml-4 xl:ml-6'>
+              <p className='text-2xl xl:text-3xl'>{group.name}</p>
+              <p className='text-base xl:text-xl mb-1 xl:mt-1'>{group.description}</p>
             </article>
-            {role === "Teacher" && <Link to="/settings " className="absolute right-6 top-4">
-              <FaCog className="m-2" />
+            {role === "Teacher" && <Link to="/settings " className="absolute right-4 xl:right-6 top-2 xl:top-4">
+              <FaCog className="m-2 text-sm xl:text-base" />
             </Link>}
-            {role === "Teacher" && <Link to={"assignments"} state={{ groupReport: true }} className='absolute bottom-2 right-8 underline underline-offset-4'>Stwórz raport grupy</Link>}
+            {role === "Teacher" && <Link to={"assignments"} state={{ groupReport: true }} className='absolute bottom-2 right-6 xl:right-8 text-sm xl:text-base underline underline-offset-4'>Stwórz raport grupy</Link>}
           </div>
         </div>
-        <header className='flex inline-block mt-4 mx-[7.5%] justify-between'>
-          <div className="text-[22px] font-lato font-medium text-center text-font_gray">
+        <header className='flex inline-block mt-4 mx-[7.5%]'>
+          <div className="text-base xl:text-xl font-lato font-medium text-center text-font_gray">
             <ul className="flex flex-wrap -mb-px">
               <li
                 className={(locationSplit[3] === "assignments" && locationSplit[4] === undefined) || locationSplit[3] === "solutions" ? "border-main_blue border-b-[3px] rounded bg-main_blue bg-opacity-5 text-black" : "hover:bg-hover_gray"}>
-                <Link to="assignments" className="inline-block w-[170px] p-4  rounded-t-lg "> Zadania</Link>
+                <Link to="assignments" className="inline-block w-28 xl:w-36 p-4  rounded-t-lg " onClick={() => setSelectedOption({ link: "assignments", label: "Wszystkie" })}> Zadania</Link>
               </li>
               <li
                 className={locationSplit[3] === "users" ? "border-main_blue border-b-[3px] rounded bg-main_blue bg-opacity-5 text-black" : "hover:bg-hover_gray"}>
-                <Link to="users" className="inline-block w-[170px] p-4  rounded-t-lg "> Osoby </Link>
+                <Link to="users" className="inline-block w-28 xl:w-36 p-4  rounded-t-lg "> Osoby </Link>
               </li>
-              {role === "Student" ?
+              {role === "Student" &&
                 <li
                   className={locationSplit[3] === "evaluations" ? "border-main_blue border-b-[3px] rounded bg-main_blue bg-opacity-5 text-black" : "hover:bg-hover_gray"}>
-                  <Link to="evaluations" className="inline-block w-[170px] p-4  rounded-t-lg "> Oceny</Link>
-                </li>
-                : <li
-                  className={locationSplit[4] === "add" ? "border-main_blue border-b-[3px] rounded bg-main_blue bg-opacity-5 text-black" : "hover:bg-hover_gray"}>
-                  <Link to="assignments/add" className="inline-block w-[205px] p-4  rounded-t-lg "> Nowe Zadanie +</Link>
+                  <Link to="evaluations" className="inline-block w-28 xl:w-36 p-4  rounded-t-lg "> Oceny</Link>
                 </li>}
             </ul>
           </div>
-          <div className='relative float-right flex h-16 items-center gap-5 '>
+          {role === "Teacher" && <div className="flex ml-5 xl:ml-10 items-center">
+            <Link to="assignments/add" >
+              <button type="button"
+                className=' border-main_blue h-8 rounded-md border-2 px-3 xl:px-4 xl:h-10 xl:rounded-lg text-main_blue text-sm xl:text-lg hover:bg-hover_gray hover:shadow-md active:bg-opacity-60'
+              >Stwórz zadanie +</button>
+            </Link>
+          </div>}
+          <div className='relative mr-0 ml-auto flex items-center'>
             <select
-              className="flex border-2 border-main_blue text-main_blue pl-2 rounded-md xl:rounded-lg w-44 xl:w-56 h-8 xl:h-10 items-center"
+              className="flex border-2 border-main_blue text-main_blue text-sm xl:text-lg pl-2 rounded-md xl:rounded-lg w-44 xl:w-56 h-8 xl:h-10 items-center"
               value={selectedOption.label}
               onChange={handleChange}
             >
