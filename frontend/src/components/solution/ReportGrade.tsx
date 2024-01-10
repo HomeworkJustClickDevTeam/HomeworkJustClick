c70bc27ed2dcad5860a0e7c5150a7b5035b11f00
Aleksandra Muczyńska <a.muczynska99@gmail.com>
2024-01-10 10:23:30 +0100

    Quickfixed Report button

--- frontend/src/components/solution/ReportGrade.tsx
+++ frontend/src/components/solution/ReportGrade.tsx
@@ -34,9 +34,10 @@ export const ReportGrade = (props:
     }
 
     return (
-        <>
+        <div >
             {isButtonClicked ? (
-                <div className=' ml-8 border-2 border-light_gray px-4 py-2  min-w-[400px] flex flex-col rounded-md text-bottom shadow-md text-center bg-white h-[260px]'>
+                <div className='fixed left-[50%] top-[30%] translate-x-[-50%] translate-y-[50%]'>
+                <div className='relative ml-8 border-2 border-light_gray px-4 py-2  min-w-[400px] flex flex-col rounded-md text-bottom shadow-md text-center bg-white h-[260px]'>
                     <p className='mb-3 font-semibold'>Zgłoś ocenę</p>
                     <div className='flex'>
                         <div>
@@ -46,14 +47,14 @@ export const ReportGrade = (props:
                                   className='min-h-[200px] border rounder-sm border-border_gray ml-3 pl-2 pr-1 max-h-[200px]' onChange={(event) => changeComment(event)} value={comment}/>
                             </label>
                             <button className='h-fit absolute bottom-2 right-2 px-3 py-1 bg-main_blue text-white rounded' onClick={reportGrade}>Wyślij </button>
-                            <button className='h-fit absolute top-2 right-2 h-fit px-3 py-1 bg-berry_red text-white rounded' onClick={() => setIsButtonClicked(false)}>X</button>
+                            <button className='h-fit absolute top-2 right-2  px-3 py-1 bg-berry_red text-white rounded' onClick={() => setIsButtonClicked(false)}>X</button>
                         </div>
-
+                    </div>
                     </div>
                 </div>
             ) : (
                 <button className='fixed 2xl:right-[calc(7.5%+25px)] right-[calc(7.5%+125px)] 2xl:top-[425px] xl:mb-0 px-6 py-1 bg-berry_red text-white rounded ' onClick={() => setIsButtonClicked(true)}>Zgłoś ocenę</button>
             )}
-        </>
+        </div>
     )
 }
