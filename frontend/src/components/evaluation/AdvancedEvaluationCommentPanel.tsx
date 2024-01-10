05961c8c95f0864f8360a532e5abf18cc3e7894a
Piotr Szymkowiak <piotr141097@gmail.com>
2024-01-10 09:02:10 +0100

    Fix direction of arrows

--- frontend/src/components/evaluation/AdvancedEvaluationCommentPanel.tsx
+++ frontend/src/components/evaluation/AdvancedEvaluationCommentPanel.tsx
@@ -1,5 +1,5 @@
 import { CommentInterface } from "../../types/CommentInterface"
-import React, {MutableRefObject, SetStateAction, useCallback, useLayoutEffect, useRef, useState} from "react"
+import React, { MutableRefObject, SetStateAction, useCallback, useLayoutEffect, useRef, useState } from "react"
 import { AdvancedEvaluationCommentPanelListElement } from "./AdvancedEvaluationCommentPanelListElement"
 import { useAppSelector } from "../../types/HooksRedux"
 import { selectUserState } from "../../redux/userStateSlice"
@@ -13,24 +13,24 @@ import { ca } from "date-fns/locale"
 import { type } from "os"
 import { parseISO } from "date-fns"
 import { FaSort } from "react-icons/fa";
-import {SortCommentsButtonStateType} from "../../types/SortCommentsButtonStateType";
+import { SortCommentsButtonStateType } from "../../types/SortCommentsButtonStateType";
 import { IoMdArrowDropright, IoMdArrowDropleft } from "react-icons/io";
 
-interface AdvancedEvaluationCommentPanelPropsInterface{
-  setChosenComment: React.Dispatch<React.SetStateAction<CommentInterface|undefined>>,
-  setUpdatedComment: React.Dispatch<React.SetStateAction<CommentInterface|undefined>>,
-  chosenCommentId:number|undefined,
-  setSortButtonState:(buttonState:SortCommentsButtonStateType) => void,
-  highlightedCommentId:number|undefined,
+interface AdvancedEvaluationCommentPanelPropsInterface {
+  setChosenComment: React.Dispatch<React.SetStateAction<CommentInterface | undefined>>,
+  setUpdatedComment: React.Dispatch<React.SetStateAction<CommentInterface | undefined>>,
+  chosenCommentId: number | undefined,
+  setSortButtonState: (buttonState: SortCommentsButtonStateType) => void,
+  highlightedCommentId: number | undefined,
   sortButtonState: SortCommentsButtonStateType,
   rightPanelUserComments: CommentInterface[],
-  setRightPanelUserComments: (comments:CommentInterface[]) => void,
+  setRightPanelUserComments: (comments: CommentInterface[]) => void,
   assignmentId: number,
-  setDeletedRightPanelCommentId: React.Dispatch<React.SetStateAction<number|undefined>>
-  onCommentPanelListRefChange: (node:HTMLDivElement)=>void
+  setDeletedRightPanelCommentId: React.Dispatch<React.SetStateAction<number | undefined>>
+  onCommentPanelListRefChange: (node: HTMLDivElement) => void
 }
 export const AdvancedEvaluationCommentPanel = (
-  {assignmentId,
+  { assignmentId,
     setUpdatedComment,
     setDeletedRightPanelCommentId,
     highlightedCommentId,
@@ -40,12 +40,12 @@ export const AdvancedEvaluationCommentPanel = (
     rightPanelUserComments,
     setRightPanelUserComments,
     setChosenComment,
-    onCommentPanelListRefChange}:AdvancedEvaluationCommentPanelPropsInterface) => {
+    onCommentPanelListRefChange }: AdvancedEvaluationCommentPanelPropsInterface) => {
 
   const [isHidden, setIsHidden] = useState<boolean>(false)
   const [newCommentDescription, setNewCommentDescription] = useState<string>("")
   const userState = useAppSelector(selectUserState)
-  const updateCommentsList = async (changedComment:CommentInterface) => {
+  const updateCommentsList = async (changedComment: CommentInterface) => {
     try {
       const response = await changeCommentPostgresService(changedComment)
       if (response !== undefined && response !== null && response.data !== undefined && response.status === 200) {
@@ -56,11 +56,11 @@ export const AdvancedEvaluationCommentPanel = (
         setRightPanelUserComments(newComments)
         setUpdatedComment(changedComment)
       }
-    }catch (error){
+    } catch (error) {
       console.log(error)
     }
   }
-  const handleCommentRemoval = async (commentId:number) => {
+  const handleCommentRemoval = async (commentId: number) => {
     try {
       const response = await deleteCommentPostgresService(commentId.toString())
       if (response.data !== null && response.data !== undefined && response.status === 200) {
@@ -72,28 +72,28 @@ export const AdvancedEvaluationCommentPanel = (
       console.log(error)
     }
   }
-  const handleNewCommentCreation = async (event:React.FormEvent<HTMLFormElement>) => {
+  const handleNewCommentCreation = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    if(newCommentDescription.length > 0)
-    {
-      if(userState?.id !== null && userState?.id !== undefined){
-        const newComment:CommentCreateInterface = {
+    if (newCommentDescription.length > 0) {
+      if (userState?.id !== null && userState?.id !== undefined) {
+        const newComment: CommentCreateInterface = {
           description: newCommentDescription,
-          title:"",
-          color: "#" + Math.floor(Math.random()*16777215).toString(16), //random color
-          userId:userState.id,
-          assignmentId:assignmentId
+          title: "",
+          color: "#" + Math.floor(Math.random() * 16777215).toString(16), //random color
+          userId: userState.id,
+          assignmentId: assignmentId
         }
         createCommentWithUserPostgresService(newComment)
           .then((response) => {
-            if(response.data !== undefined && response.data !== null && response.status === (201 || 200)){
+            if (response.data !== undefined && response.data !== null && response.status === (201 || 200)) {
               const tempComments = rightPanelUserComments
               setRightPanelUserComments([{
                 ...newComment,
-                id:response.data.id,
-                counter:response.data.counter,
-                lastUsedDate: parseISO(response.data.lastUsedDate)},
-                ...tempComments])
+                id: response.data.id,
+                counter: response.data.counter,
+                lastUsedDate: parseISO(response.data.lastUsedDate)
+              },
+              ...tempComments])
               setNewCommentDescription("")
             }
           })
@@ -104,34 +104,34 @@ export const AdvancedEvaluationCommentPanel = (
 
 
   return (
-    <div  style={{float:"right", height:"100vh"}} className='flex'>
-      <button className={'bg-light_gray h-full w-[20px] pl-[2px] rounded-bl-md rounded-tl-md'} onClick={() => setIsHidden(!isHidden)}>{isHidden ? <IoMdArrowDropright/> : <IoMdArrowDropleft />}</button>
+    <div style={{ float: "right", height: "100vh" }} className='flex'>
+      <button className={'bg-light_gray h-full w-[20px] pl-[2px] rounded-bl-md rounded-tl-md'} onClick={() => setIsHidden(!isHidden)}>{isHidden ? <IoMdArrowDropleft /> : <IoMdArrowDropright />}</button>
       {!isHidden && <div className='pl-1 w-[310px]'>
-          <p className='text-center underline underline-offset-4 mb-4'>KOMENTARZE</p>
+        <p className='text-center underline underline-offset-4 mb-4'>KOMENTARZE</p>
 
-          <form onSubmit={(event) => handleNewCommentCreation(event)}>
+        <form onSubmit={(event) => handleNewCommentCreation(event)}>
           <div className='inline-flex w-full pb-2'>
             <p className='mr-2'>Dodaj komentarz: </p>
-            <input value={newCommentDescription} className='border border-black rounded-sm mr-2 w-full pl-1.5 h-10 mt-2' onChange={(event) => setNewCommentDescription(event.target.value)}/>
-            <button className='w-24 ml-auto mr-2 bg-main_blue text-white px-2 h-10 mt-2 rounded-md' type={"submit"}>Dodaj</button><br/><hr/>
+            <input value={newCommentDescription} className='border border-black rounded-sm mr-2 w-full pl-1.5 h-10 mt-2' onChange={(event) => setNewCommentDescription(event.target.value)} />
+            <button className='w-24 ml-auto mr-2 bg-main_blue text-white px-2 h-10 mt-2 rounded-md' type={"submit"}>Dodaj</button><br /><hr />
           </div>
         </form>
         <section >
           <p className='pt-1 pb-1 font-semibold' ref={onCommentPanelListRefChange}>Wybierz komentarz: </p>
-            <section className='border border-border_gray rounded-md w-fit px-2 flex'>
-              <label htmlFor={"sortDropdown"} className='flex'> <FaSort className='mt-1'/>Sortuj:</label>
-              <select defaultValue={sortButtonState} onChange={(event) => setSortButtonState(event.target.value as SortCommentsButtonStateType)} name={"sortDropdown"} id={"sortDropdown"}>
-                <option value={"description,asc"}>Alfabetycznie</option>
-                <option value={"counter,desc"}>Najczęściej używane</option>
-                <option value={"counter,asc"}> Najrzadziej używane</option>
-                <option value={"lastUsedDate,desc"}>Ostatnio używane </option>
-              </select>
+          <section className='border border-border_gray rounded-md w-fit px-2 flex'>
+            <label htmlFor={"sortDropdown"} className='flex'> <FaSort className='mt-1' />Sortuj:</label>
+            <select defaultValue={sortButtonState} onChange={(event) => setSortButtonState(event.target.value as SortCommentsButtonStateType)} name={"sortDropdown"} id={"sortDropdown"}>
+              <option value={"description,asc"}>Alfabetycznie</option>
+              <option value={"counter,desc"}>Najczęściej używane</option>
+              <option value={"counter,asc"}> Najrzadziej używane</option>
+              <option value={"lastUsedDate,desc"}>Ostatnio używane </option>
+            </select>
 
-            </section>
-          <br/>
-            <hr className='mr-8 mb-2'/>
+          </section>
+          <br />
+          <hr className='mr-8 mb-2' />
           <div className='overflow-y-auto mx-4'>{rightPanelUserComments.map((comment) => {
-            return(
+            return (
               <div key={comment.id} >
                 <AdvancedEvaluationCommentPanelListElement
                   commentId={comment.id}
@@ -141,7 +141,8 @@ export const AdvancedEvaluationCommentPanel = (
                   comment={comment}
                   setChosenComment={setChosenComment}
                   updateCommentsList={updateCommentsList}></AdvancedEvaluationCommentPanelListElement>
-              </div>)}
+              </div>)
+          }
           )}</div>
         </section>
       </div>}
--- frontend/src/components/solution/SolutionCheckedAdvancedPage.tsx
+++ frontend/src/components/solution/SolutionCheckedAdvancedPage.tsx
@@ -1,28 +1,28 @@
-import {Link, useLocation} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import {SolutionExtendedInterface} from "../../types/SolutionExtendedInterface";
-import {useGetFile} from "../customHooks/useGetFile";
-import {useDetermineFileType} from "../customHooks/useDetermineFileType";
-import {useGetCommentsImageByFile} from "../customHooks/useGetCommentsImageByFile";
-import {useGetCommentsTextByFile} from "../customHooks/useGetCommentsTextByFile";
-import {useGetSolutionAreaSizeAvailable} from "../customHooks/useGetSolutionAreaSizeAvailable";
-import {AdvancedEvaluationTextCommentModel} from "../../types/AdvancedEvaluationTextComment.model";
-import {AdvancedEvaluationImageCommentModel} from "../../types/AdvancedEvaluationImageComment.model";
-import {AdvancedEvaluationCommentPanelListElement} from "../evaluation/AdvancedEvaluationCommentPanelListElement";
-import {AdvancedEvaluationImageArea} from "../evaluation/AdvancedEvaluationImageArea";
-import {useUpdateEffect} from "usehooks-ts";
-import {AdvancedEvaluationTextArea} from "../evaluation/AdvancedEvaluationTextArea";
-import {IoIosArrowBack, IoMdArrowDropleft, IoMdArrowDropright} from "react-icons/io";
+import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { SolutionExtendedInterface } from "../../types/SolutionExtendedInterface";
+import { useGetFile } from "../customHooks/useGetFile";
+import { useDetermineFileType } from "../customHooks/useDetermineFileType";
+import { useGetCommentsImageByFile } from "../customHooks/useGetCommentsImageByFile";
+import { useGetCommentsTextByFile } from "../customHooks/useGetCommentsTextByFile";
+import { useGetSolutionAreaSizeAvailable } from "../customHooks/useGetSolutionAreaSizeAvailable";
+import { AdvancedEvaluationTextCommentModel } from "../../types/AdvancedEvaluationTextComment.model";
+import { AdvancedEvaluationImageCommentModel } from "../../types/AdvancedEvaluationImageComment.model";
+import { AdvancedEvaluationCommentPanelListElement } from "../evaluation/AdvancedEvaluationCommentPanelListElement";
+import { AdvancedEvaluationImageArea } from "../evaluation/AdvancedEvaluationImageArea";
+import { useUpdateEffect } from "usehooks-ts";
+import { AdvancedEvaluationTextArea } from "../evaluation/AdvancedEvaluationTextArea";
+import { IoIosArrowBack, IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 
 
 export const SolutionCheckedAdvancedPage = () => {
-    const {state} = useLocation()
+    const { state } = useLocation()
     const [solutionExtended, setSolutionExtended] = useState<SolutionExtendedInterface>(state.solutionExtended)
     const [isHidden, setIsHidden] = useState<boolean>(false)
     const solutionFile = useGetFile(solutionExtended.id, "solution")
-    const {image, fileText} = useDetermineFileType(solutionFile)
-    const {commentsImage, setCommentsImage} = useGetCommentsImageByFile(solutionFile?.id, "")
-    const {comments: commentsText, setComments: setCommentsText} = useGetCommentsTextByFile(solutionFile?.id, "")
+    const { image, fileText } = useDetermineFileType(solutionFile)
+    const { commentsImage, setCommentsImage } = useGetCommentsImageByFile(solutionFile?.id, "")
+    const { comments: commentsText, setComments: setCommentsText } = useGetCommentsTextByFile(solutionFile?.id, "")
     const [highlightedCommentId, setHighlightedCommentId] = useState<number | undefined>(undefined)
     const {
         availableHeight,
@@ -48,15 +48,15 @@ export const SolutionCheckedAdvancedPage = () => {
 
             return <div ref={onCommentPanelListRefChange} key={drawnComment.id}>
                 <AdvancedEvaluationCommentPanelListElement highlightedCommentId={highlightedCommentId}
-                                                           comment={drawnComment.comment}
-                                                           commentId={drawnComment.comment.id}></AdvancedEvaluationCommentPanelListElement>
+                    comment={drawnComment.comment}
+                    commentId={drawnComment.comment.id}></AdvancedEvaluationCommentPanelListElement>
             </div>
         })}
         </div>)
     }
     const showPanel = () => {
         return (<div className='pl-1 w-[310px]'>
-            Komentarze: <br/>
+            Komentarze: <br />
             {image !== undefined ? showRightPanelComments(commentsImage)
                 :
                 fileText !== undefined && showRightPanelComments(commentsText)}
@@ -74,26 +74,26 @@ export const SolutionCheckedAdvancedPage = () => {
         setSolutionExtended(state.solutionExtended)
     }, [state]);
     return (<div>
-        <div style={{width: "100%"}} ref={onBackButtonRefChange}>
+        <div style={{ width: "100%" }} ref={onBackButtonRefChange}>
             <Link to={`/group/${solutionExtended.assignment.groupId}/assignment/${solutionExtended.assignment.id}`}
-                  className='flex underline underline-offset-2'><IoIosArrowBack className='ml-2 mr-1 mt-1'/>Wróć</Link>
+                className='flex underline underline-offset-2'><IoIosArrowBack className='ml-2 mr-1 mt-1' />Wróć</Link>
         </div>
-        <div style={{float: "right", height: "100vh"}} className='flex'>
+        <div style={{ float: "right", height: "100vh" }} className='flex'>
             <button className={'bg-light_gray h-full w-[20px] pl-[2px] rounded-bl-md rounded-tl-md'}
-                    onClick={() => setIsHidden(!isHidden)}>{isHidden ? <IoMdArrowDropright/> :
-                <IoMdArrowDropleft/>}</button>
+                onClick={() => setIsHidden(!isHidden)}>{isHidden ? <IoMdArrowDropleft /> :
+                    <IoMdArrowDropright />}</button>
             {!isHidden && showPanel()}
         </div>
         <div>
             {image !== undefined ?
                 <AdvancedEvaluationImageArea width={availableWidth} height={availableHeight} image={image}
-                                             commentsImage={commentsImage}
-                                             handleCommentHighlighting={handleCommentHighlighting}></AdvancedEvaluationImageArea>
+                    commentsImage={commentsImage}
+                    handleCommentHighlighting={handleCommentHighlighting}></AdvancedEvaluationImageArea>
                 :
                 fileText !== undefined &&
                 <AdvancedEvaluationTextArea handleCommentHighlighting={handleCommentHighlighting} width={availableWidth}
-                                            height={availableHeight} comments={commentsText} fileText={fileText}
-                                            commentsText={commentsText}/>}
+                    height={availableHeight} comments={commentsText} fileText={fileText}
+                    commentsText={commentsText} />}
         </div>
     </div>)
 }
